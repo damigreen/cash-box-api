33611a8a0a06b3500461b29c39a4ec09fdd0dea3
Damilola Faseun <damilola.faseun@gmail.com>
2022-03-13 21:46:54 +0100

    environment contracts

--- env.ts
+++ env.ts
@@ -22,9 +22,9 @@ export default Env.rules({
 	DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
 	
-	MSSQL_SERVER: Env.schema.string({ format: 'host' }),
-	MSSQL_PORT: Env.schema.number(),
-	MSSQL_USER: Env.schema.string(),
-	MSSQL_PASSWORD: Env.schema.string.optional(),
-	MSSQL_DB_NAME: Env.schema.string(),
+	MYSQL_SERVER: Env.schema.string({ format: 'host' }),
+	MYSQL_PORT: Env.schema.number(),
+	MYSQL_USER: Env.schema.string(),
+	MYSQL_PASSWORD: Env.schema.string.optional(),
+	MYSQL_DB_NAME: Env.schema.string(),
 })
