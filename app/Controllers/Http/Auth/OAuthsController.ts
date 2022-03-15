75695756865566dac281d8753d06ff0cde3db1d4
Damilola Faseun <damilola.faseun@gmail.com>
2022-03-15 10:20:23 +0100

    Login logic first attempt

--- app/Controllers/Http/Auth/OAuthsController.ts
+++ app/Controllers/Http/Auth/OAuthsController.ts
@@ -1,17 +1,40 @@
-import { validator } from './../../../../config/app';
+// import { Exception } from '@adonisjs/core/build/standalone';
+// import { validator } from './../../../../config/app';
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from "App/Models/User";
+import CtxExtendContract from "Contracts/ctxExtend";
+
 
 class OAuthsController {
-    async login({ request, response, validator }) {
-        const { username, password } = await validator.validate({
+    async login({ auth, request, response, validator }: CtxExtendContract) {
+        const { username, password, email } = await validator.validate({
+            // email: validator.schema.string(),
             username: validator.schema.string(),
             password: validator.schema.string(),
         })
+
         console.log(username, password);
 
+        
+        try {
+            const x = await User.query()
+                // .where("active", true)
+                .where("email", username)
+                .orWhere("username", username)
+                .firstOrFail();
+
+                console.log(x);
+
+        } catch (error) {
+            // throw new Exception()
+            console.log(error);
+        }
+        const token = await auth.use('api').attempt(email, password);
+        
         response.json({
             status: true,
-        })
+            token,
+        });
     }
 }
 
